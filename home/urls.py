493745f7be21fdbb420f34b288238864d96500a4
Nabin-Flash320 <nabindangi32@gmail.com>
2022-06-27 17:28:12 +0545

    Static files.

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,17 @@
 
 
+from unicodedata import name
 from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('price/', views.price, name='price'),
+    path('services/', views.services, name='services'),
 ]
 
 
