37b420af34b221b4d881ea789a81d86ca18c0ba0
Nabin-Flash320 <nabindangi32@gmail.com>
2022-06-28 17:12:08 +0545

    Created first model and migrated it!

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=200)
+    subject = models.TextField()
+    message = models.TextField()
+
+
